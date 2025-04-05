4b60d9aa10a353aeb0d7bd06a7ecddeaa37010f1
rose <94741173+pacelika@users.noreply.github.com>
2025-04-05 14:47:51 -0700

    removed config.org

---
+++ compile.el
@@ -0,0 +1,42 @@
+(require 'compile)
+
+(setq compilation-scroll-output t)
+(defvar last-compile-command nil)
+
+(defun get-common-project-compile-command(root)
+  "get the project common compile command"
+  (cond
+   ((file-exists-p (expand-file-name "Cargo.toml" root)) "cargo run")
+   ((file-exists-p (expand-file-name "package.json" root)) "npm run dev")
+   ((file-exists-p (expand-file-name "build.zig" root)) "zig build run")
+   ((file-exists-p (expand-file-name "Makefile" root)) "make")
+   ((file-exists-p (expand-file-name "CMakeLists.txt" root)) "cmake -S .")
+   (t nil)))
+
+(defun find-project-root()
+  "Find the root of the project by searching for .git, Makefile, or similar markers."
+  (or (locate-dominating-file default-directory ".git")
+      (locate-dominating-file default-directory "Makefile")
+      (locate-dominating-file default-directory "CMakeLists.txt")
+      (locate-dominating-file default-directory "package.json")
+      (locate-dominating-file default-directory "Cargo.toml")
+      (locate-dominating-file default-directory "build.zig")))
+
+(defun compile-root-without-confirmation()
+  "Run `compile` without command prompt."
+  (interactive)
+  (let ((default-directory (or (find-project-root) default-directory)))
+    (compile last-compile-command)))
+
+(defun compile-root-with-prompt()
+  "Prompt for a compile command and run it from the project root."
+  (interactive)
+  (let* ((root (or (find-project-root) default-directory))
+         (default-command (or last-compile-command (get-common-project-compile-command root)))
+         (command (read-shell-command "Compile command: " default-command)))
+    (setq last-compile-command command)
+    (let ((default-directory root))
+      (compile command))))
+
+(global-set-key (kbd "C-c C-/") 'compile-root-with-prompt)
+(global-set-key (kbd "C-c C-.") 'compile-root-without-confirmation)
---
+++ config.el
@@ -0,0 +1,24 @@
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(defun goto-config-file() (interactive) (find-file (expand-file-name "~/.emacs.d/config.el")))
+(keymap-global-set "C-x C-\\" 'goto-config-file)
+
+(with-eval-after-load 
+    (message (concat "Ermm Emacs took: " (emacs-init-time) " to initialize")))
+
+(use-package expand-region
+  :ensure t
+  :defer 2
+  :bind ("C-=" . er/expand-region))
+
+(load "~/.emacs.d/defaults.el")
+(load "~/.emacs.d/compile.el")
+(load "~/.emacs.d/languages.el")
+(load "~/.emacs.d/lsp.el")
