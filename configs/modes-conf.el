77fef226f37087c8d449d112fcf933833ca78066
rose <94741173+pacelika@users.noreply.github.com>
2025-04-27 20:02:17 -0700

    added lua-mode

--- configs/modes-conf.el
+++ configs/modes-conf.el
@@ -1,3 +1,6 @@
+(use-package rust-mode :ensure t :defer t)
+(use-package lua-mode :ensure t :defer t)
+
 (let ((dir (expand-file-name "~/.emacs.d/modes")))
   (dolist (file (nthcdr 0 (directory-files dir t "\\.el$")))
     (load file)))
