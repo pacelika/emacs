52528397169abcc8b1bb0e50d7de4fc45e9de858
rose <94741173+pacelika@users.noreply.github.com>
2025-04-28 22:25:40 -0700

    added fennel-mode

--- configs/modes-conf.el
+++ configs/modes-conf.el
@@ -1,5 +1,17 @@
-(use-package rust-mode :ensure t :defer t)
-(use-package lua-mode :ensure t :defer t)
+(use-package rust-mode 
+  :ensure t 
+  :defer t)
+
+(use-package lua-mode 
+  :ensure t 
+  :defer t)
+
+(use-package fennel-mode 
+  :ensure t 
+  :defer t
+  :bind (("C-c C-c" . #'fennel-eval-region))
+  :config 
+  (setq fennel-program "lua fennel.lua --repl"))
 
 (let ((dir (expand-file-name "~/.emacs.d/modes")))
   (dolist (file (nthcdr 0 (directory-files dir t "\\.el$")))
