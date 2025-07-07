2139da0a46c6b3c48b44ac7285f5af98a355e219
pacelika <tamtammacro@gmail.com>
2025-07-07 05:11:58 -0700

    added ement

--- config/elcord.el
+++ config/elcord.el
@@ -1,9 +1,9 @@
-(add-to-list 'load-path "~/.emacs.d/modes/elcord")
-(autoload 'elcord-mode "~/.emacs.d/modes/elcord/elcord.el" nil t)
+;; (add-to-list 'load-path "~/.emacs.d/modes/elcord")
+;; (autoload 'elcord-mode "~/.emacs.d/modes/elcord/elcord.el" nil t)
 
-(require 'elcord)
+;; (require 'elcord)
 
-(use-package elcord
-  :defer t
-  :init
-  (elcord-mode))
+;; (use-package elcord
+;;   :defer t
+;;   :init
+;;   (elcord-mode))
